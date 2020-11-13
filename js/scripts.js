c4179316f5d1d14b1e86a5b207c1926d815031ec
Annette Young <annetteyoung@Annettes-MBP.home>
2020-11-12 20:42:29 -0800

    some updates in jquery, button is working, images are not

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 jQuery(function() {
 
+  let my_switch = false;
+
 	$('#my_button').click(function () {
+		if ( my_switch == false ) {
+		 my_switch = true;
+		 $(this).html('Donuts! Or... !');
+			$('my_image').attr('src', 'images/boys2.jpg');
+		} else {
+			my_switch = false;
+			$(this).html('Ice Cream!');
+			$('my_image').attr('src', 'images/boys3.jpg');
+
+		}
 	});
 
 });
