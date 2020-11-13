c5e36f335284affed51f716a6eccfd05a4696905
Annette Young <annetteyoung@Annettes-MBP.home>
2020-11-12 20:44:49 -0800

    button fix

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,11 @@ jQuery(function() {
 	$('#my_button').click(function () {
 		if ( my_switch == false ) {
 		 my_switch = true;
-		 $(this).html('Donuts! Or... !');
+		 $(this).html('Ice Cream! Or... ');
 			$('my_image').attr('src', 'images/boys2.jpg');
 		} else {
 			my_switch = false;
-			$(this).html('Ice Cream!');
+			$(this).html('..Mmm.. Donuts!');
 			$('my_image').attr('src', 'images/boys3.jpg');
 
 		}
