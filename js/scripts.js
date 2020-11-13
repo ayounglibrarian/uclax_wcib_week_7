523a855f651012b9c2698399f1f6483546897595
Annette Young <annetteyoung@Annettes-MBP.home>
2020-11-12 22:01:48 -0800

    getting closer! fix buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,39 @@
 jQuery(function() {
+	//
+  // let my_switch = false;
+	//
+	// $('#my_button').click(function () {
+	// 	if ( my_switch == false ) {
+	// 	 my_switch = true;
+	// 	 $(this).html('Ice Cream! Or... ');
+	// 		$('my_image').attr('src', 'images/boys2.jpg');
+	// 	} else {
+	// 		my_switch = false;
+	// 		$(this).html('Mmm.. Donuts!');
+	// 		$('my_image').attr('src', 'images/boys3.jpg');
 
-  let my_switch = false;
 
-	$('#my_button').click(function () {
-		if ( my_switch == false ) {
-		 my_switch = true;
-		 $(this).html('Ice Cream! Or... ');
-			$('my_image').attr('src', 'images/boys2.jpg');
-		} else {
-			my_switch = false;
-			$(this).html('..Mmm.. Donuts!');
-			$('my_image').attr('src', 'images/boys3.jpg');
+	$('.refresh-button').on({
+	     'click': function(){
+	         $('#change-image').attr('src','images/boys1.jpg');
+	     }
+	 });
+
+	$('.ic-button').on({
+	     'click': function(){
+	         $('#change-image').attr('src','images/boys2.jpg');
+	     }
+	 });
+
+	$('.donut-button').on({
+	     'click': function(){
+	         $('#change-image').attr('src','images/boys3.jpg');
+	     }
+	 });
+
+	$('.yellow-button').on({
+	     'click': function(){
+	         $('#change-image').attr('src','/wp-content/uploads/2018/09/yellow.jpg');
 
 		}
 	});
